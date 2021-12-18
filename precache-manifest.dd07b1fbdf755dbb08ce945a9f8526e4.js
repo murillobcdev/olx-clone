37012e6079b6e2d13479831ba719f150a3f559f7
Murillo Brito <murillobdev@gmail.com>
2021-12-18 17:26:00 -0300

    Updates

---
+++ precache-manifest.dd07b1fbdf755dbb08ce945a9f8526e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bbfe09c938c93fe51194b925a1ee8c",
+    "url": "/mbstore/index.html"
+  },
+  {
+    "revision": "74015dd60db29ee75587",
+    "url": "/mbstore/static/css/2.0d4de01b.chunk.css"
+  },
+  {
+    "revision": "86b7db8c99dbedf31c48",
+    "url": "/mbstore/static/css/main.fc3dc48f.chunk.css"
+  },
+  {
+    "revision": "74015dd60db29ee75587",
+    "url": "/mbstore/static/js/2.011c4095.chunk.js"
+  },
+  {
+    "revision": "86b7db8c99dbedf31c48",
+    "url": "/mbstore/static/js/main.e3ddec85.chunk.js"
+  },
+  {
+    "revision": "cdfb8cdac018c9a6c0ea",
+    "url": "/mbstore/static/js/runtime-main.910e2a6f.js"
+  }
+]);
